ab36db0b9da5910a07cabca4808cc087046bfc35
Stig-Jane <hnordlander@gmail.com>
2017-11-24 14:55:20 +0100

    skapa led/företag

--- includes/views/my_trails.inc.php
+++ includes/views/my_trails.inc.php
@@ -5,7 +5,7 @@
 
 
 
-	$sql = "SELECT * FROM org_profile join (SELECT trail.trailID, SUM(vote) as 'rating' FROM vote,org_profile WHERE vote.trailID = trail.trailID group by trail.trailID) rate on trail.trailID = rate.trailID WHERE trail.userID='$loged_in'";
+	$sql = "SELECT * FROM org_profile join (SELECT org_profile.trailID, SUM(vote) as 'rating' FROM vote,org_profile WHERE vote.trailID = org_profile.trailID group by org_profile.trailID) rate on org_profile.trailID = rate.trailID WHERE org_profile.userID='$loged_in'";
 	$result = mysqli_query($conn, $sql);
 
 		while ($row = $result->fetch_assoc())  //Runs through the entire result
@@ -18,7 +18,7 @@
 			echo "<img src='assets/img/maps_icon.png' class='trail_img'><div class='trail_content_info'>";
 			echo "<label id='trail_name_in_list'>" . $row['trailName'] . "</label> <br>	";
 			echo "<label class='trail_list_difficulty'>Svårighet:<label>" . $row['difficultyLevel'] . "<br>";
-			echo "<label class='trail_list_length'>Längd:<label>" . $row['trailLength'] . "<br>";
+			
 			
 			echo "</div>";
 			
--- includes/views/trail_info.php
+++ includes/views/trail_info.php
@@ -8,11 +8,9 @@
 
 			<input name='trail_name_value' id='trail_name' placeholder='Namge din led'>
 			
-			<label>Svårighet</label>
-			<input name='trail_difficulty_value' id='trail_difficulty' type='range' min='1' max='5' value='1'  onchange='updateTextInput(this.value);'>
-			<input type='text' id='trail_current_difficulty' value='Nybörjare'>
 
-			<input name='trail_lenght_value' id='trail_length' placeholder='ledlängd'>
+
+			
 			
 			<input type='hidden' name='trail_creation_date_value' id='trail_creation_date' value='".date('Y-m-d H:i:s')."'> 
 			<textarea name='trail_info_value' id='trail_info' placeholder='Ange information om led' ></textarea>

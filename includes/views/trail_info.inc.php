75a42e101ba7b5f449c0879558e6568f9905c507
Stig-Jane <hnordlander@gmail.com>
2017-11-29 16:11:33 +0100

    123

--- includes/views/trail_info.inc.php
+++ includes/views/trail_info.inc.php
@@ -3,9 +3,14 @@
  -->
 <?php
 	$trail_input = "
-
+			<div id='name_orgno_container'>
 			<input name='trail_name_value' id='trail_name' placeholder=' Företagsnamn'>
-			<input name='org_name_value' id='org_no' placeholder=' xxxxxx-xxxx'>
+			<input name='org_name_value' id='org_no' placeholder=' xxxxxx-xxxx'></div>
+			<label id='site_adress_topic'>Adress</label>
+			<div id='site_adress_container'>
+			<input id='org_street_value' placeholder='Gatunamn'>
+			<input id='org_postno_value' placeholder='Postnummer'>
+			<input id='org_city_value' placeholder='Postort'></div>
 			<div id='attributes_container'>
 			<label id='sites_topic'>Siter</label>
 			<div id='att_add_button'></div>

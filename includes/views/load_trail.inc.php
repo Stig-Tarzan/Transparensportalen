843ead04aef842a933e0f97e33dfa95164666a36
Stig-Tarzan <john.f.almqvist@gmail.com>
2017-11-29 14:41:26 +0100

    städjobb

--- includes/views/load_trail.inc.php
+++ includes/views/load_trail.inc.php
@@ -7,35 +7,23 @@
 <?php
   session_start(); 
   include '../bootstrap.php';
-  $trail_creator = $_POST['trail_creator'];
-  $selected_trail = $_POST['selected_trail'];
+  
   $trail_name = $_POST['trail_name'];
-  $sql = "SELECT * FROM org_profile WHERE trailName = '$trail_name'";
+  $sql = "SELECT * FROM org_profile,user WHERE orgName = '$trail_name' AND org_profile.userID=user.userID";
   $result = mysqli_query($conn, $sql);
 
-/*  $sql_vote = "SELECT * FROM user,trail join (SELECT trail.trailID, SUM(vote) as 'rating' FROM vote,trail WHERE vote.trailID = trail.trailID group by trail.trailID) rate on trail.trailID = rate.trailID WHERE trail.userID=user.userID AND trail.trailName = '$trail_name'";
-  $result_vote = mysqli_query($conn, $sql_vote);
-  $row_vote = mysqli_fetch_assoc($result_vote);
-  $rating = $row_vote['rating'];*/
-
-
   $row = mysqli_fetch_assoc($result);
-  $trail_id = $row['trailID'];
-  $trail_description = $row['trailInfoText'];
-  $trail_length = $row['trailLength'];
-
-  
-
-
+  $trail_id = $row['orgID'];
+  $trail_description = $row['orgInfoText'];
+  $trail_creator = $row['userName'];
 
   echo "<input type='hidden' id='vote_trail_name' value='$trail_name'></input>";
   echo "<input type='hidden' id='vote_trail_creator_name' value='$trail_creator'></input>"; 
-
+  
   echo "<div id ='trail_information'";
   echo "<label id='trail_name_in_display' value='$trail_name'><h1>$trail_name</h1></label>";
   echo "<label id='trail_creator_name_in_display' value='$trail_creator'>Skapad av: $trail_creator</label>"; 
   echo "<text id='trail_description' value='$trail_name'>Beskrivning: $trail_description</text>";
-  echo "<text id='trail_description' value='$trail_name'>Längd: $trail_length meter</text>";
   echo "</div>";
   
    
--- includes/views/profile.inc.php
+++ includes/views/profile.inc.php
@@ -7,7 +7,6 @@ session_start();
 	{
 		$logged_in = $_SESSION['user_id'];
 		$steps = 0;
-		
 
 
 	$sql = "SELECT * FROM org_profile,user WHERE org_profile.userID=user.userID";
@@ -18,31 +17,22 @@ session_start();
 
 		while ($row = $result->fetch_assoc())  
 		{
-			$trail_name = $row['trailName'];
+			$org_name = $row['orgName'];
 
-			$sql_trail_id = "SELECT * FROM org_profile WHERE trailName='$trail_name'";
-			$result_trail_id=mysqli_query($conn, $sql_trail_id);
-			$row_trail_id=mysqli_fetch_assoc($result_trail_id);
-			$trail_id = $row_trail_id['trailID'];
-
-			$sql_rating = "SELECT SUM(vote) as 'rating' FROM vote WHERE trailID = '$trail_id'";
-			$result_rating = mysqli_query($conn, $sql_rating);
-			$row_rating = mysqli_fetch_assoc($result_rating);
-
-
-			if( $row['userID'] == $logged_in) {
-				$steps = $steps + $row_rating['rating'];
-			}
+			$sql_org_id = "SELECT * FROM org_profile WHERE orgName='$org_name'";
+			$result_org_id=mysqli_query($conn, $sql_org_id);
+			$row_org_id=mysqli_fetch_assoc($result_org_id);
+			$org_id = $row_org_id['orgID'];
 
 		}
 		echo "
-				<div id='username_welcome'>" .$_SESSION['user_name'] ."<a id='user_steg'>Steg " .$steps ."</a></div>";
+				<div id='username_welcome'>" .$_SESSION['user_name'] ."</div>";
 				if($_SESSION['admin'] == 1)
 				{
 					echo "<a id='admin_page'>Administratörssida</a>";
 				}
 		echo "
-				<a id='user_trails'>Mina Leder</a>
+				<a id='user_trails'>Mina organisationer</a>
 				<a class='banner_obj' id='log_out_button'>Logga ut </a>
 				
 				

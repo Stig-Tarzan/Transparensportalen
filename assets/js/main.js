d91e9336e5707f625470407503e5ab4bb051b618
Stig-Tarzan <john.f.almqvist@gmail.com>
2017-11-29 18:14:06 +0100

    Fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -453,7 +453,6 @@ var poly_total;
         		site_zipcode_value: site_zipcode, 
         		site_city_value: site_city,
         		org_number_value: org_number_value
-
       		}
 
       		$.post("includes/models/site_process.inc.php" , 
--- includes/models/site_process.inc.php
+++ includes/models/site_process.inc.php
@@ -4,30 +4,32 @@
 	
 
 
-	if (isset($_POST['site_list_value'])) 
+	if (isset($_POST['org_number_value'])) 
 	{
+		echo "HEJJ!";
+		$site_name = mysqli_real_escape_string ($conn,$_POST['site_name_value']);
+		$site_street = mysqli_real_escape_string ($conn,$_POST['site_street_value']);
+		$site_zipcode = mysqli_real_escape_string ($conn,$_POST['site_zipcode_value']);
+		$site_city = mysqli_real_escape_string ($conn,$_POST['site_city_value']);
+		$org_number = mysqli_real_escape_string ($conn,$_POST['org_number_value']);
+		
 
-		$site_list = mysqli_real_escape_string ($conn,$_POST['site_list_value']);
-		
-		
-		
-
-		$sql = "SELECT userID FROM user WHERE userName = '$user_name'";
+		$sql = "SELECT orgID FROM org_profile WHERE orgNumber = '$org_number'";
 		$result = mysqli_query($conn, $sql);
 
 		$row = mysqli_fetch_assoc($result);
-		$user_ID = $row['userID'];	
+		$org_ID = $row['orgID'];	
 
-		$sql = "INSERT INTO org_profile (userID, orgNumber, orgName, orgStreet, orgPostNumber, orgCity, creationDate, orgInfoText) 
-			VALUES ('$user_ID', '$org_number', '$org_name', '$org_street', '$org_postnumber', '$org_city', '$org_creation_date', '$org_info')";	
+		$sql = "INSERT INTO site (orgID, siteName, street, zipcode, city) 
+			VALUES ('$org_ID', '$site_name', '$site_street', '$site_zipcode', '$site_city')";	
 		$result = mysqli_query($conn, $sql);
 
 		if ($result) 
 		{
-			echo "Leds registrerad";
+			echo "Site registrerad";
 		}
 		else
 		{
-			echo "Led inte registrerad";
+			echo "Site inte registrerad";
 		}
 	}

7c1cba515c9906b5de30f4946d62cb8c61732de8
Stig-Tarzan <john.f.almqvist@gmail.com>
2017-11-29 17:00:52 +0100

    HEj

--- includes/models/site_process.inc.php
+++ includes/models/site_process.inc.php
@@ -4,16 +4,10 @@
 	
 
 
-	if (isset($_POST['trail_name_value'])) 
+	if (isset($_POST['site_list_value'])) 
 	{
-		$user_name = mysqli_real_escape_string ($conn,$_SESSION['user_name']);
-		$org_number = mysqli_real_escape_string ($conn,$_POST['org_number_value'])
-		$org_street = mysqli_real_escape_string ($conn,$_POST['org_street_value']);
-		$org_name = mysqli_real_escape_string ($conn,$_POST['org_name_value']);
-		$org_postnumber = mysqli_real_escape_string ($conn,$_POST['org_postnumber_value']);
-		$org_city = mysqli_real_escape_string ($conn,$_POST['org_city_value']);
-		$org_creation_date = mysqli_real_escape_string ($conn,$_POST['org_creation_date_value']);
-		$org_info = mysqli_real_escape_string ($conn,$_POST['org_info_value']);
+
+		
 
 		$sql = "SELECT userID FROM user WHERE userName = '$user_name'";
 		$result = mysqli_query($conn, $sql);

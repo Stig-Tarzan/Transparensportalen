66aa18e6c1df05f7ae57be38271345639e0aac6b
Stig-Tarzan <john.f.almqvist@gmail.com>
2017-12-13 14:49:18 +0100

    Overlay

--- includes/views/edit_attribute_info.inc.php
+++ includes/views/edit_attribute_info.inc.php
@@ -5,4 +5,4 @@
 
 	 $selected_att = mysqli_real_escape_string($conn,$_POST['selected_att']);
 
-	 echo "<div id='edit_att'><p>" .$selected_att. "<div id='inner_edit'>Quantity<input></input><list><label>source</label><input></input></div></div>";
+	 echo "<div id='overlay'><div id='edit_att'><p>" .$selected_att. "<div id='inner_edit'>Quantity<input></input><list><label>source</label><input></input></div></div></div>";
